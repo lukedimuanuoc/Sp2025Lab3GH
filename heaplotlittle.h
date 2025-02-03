ccb1705534d286096a0d67d7913a0e4620b8edd4
lukedimuanuoc <le3t2@mail.uc.edu>
2025-02-03 17:19:36 -0500

    Update heaplotlittle.h

--- heaplotlittle.h
+++ heaplotlittle.h
@@ -4,5 +4,26 @@ class measurement {
     double lots;
     double heaps;
   public:
-    measurement(){}
-    measurement(double littles)
+    measurement(){
+    }
+    measurement(double littles, double lots, double heaps){
+    }
+    measurement(total_littles
+    double getLittles() {
+      return littles;
+    }
+    void setLittles(int li) {
+      littles = li;
+    }
+    double getLots() {
+      return littles;
+    }
+    void setLots(int lo) {
+      lots = lo;
+    }
+    double getHeaps() {
+      return heaps;
+    }
+    void setHeaps(int he) {
+      heaps = he;
+    }
